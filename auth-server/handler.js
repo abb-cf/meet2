2900196147af80cf9b3851b293ad88c621ead258
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-03-03 15:19:53 -0500

    creating getCalendarEvents function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,4 +1,5 @@
 const { google } = require("googleapis");
+const { resultingClientExists } = require("workbox-core/_private");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 /**
@@ -103,4 +104,54 @@ module.exports.getAccessToken = async (event) => {
         };
       });
   };
+
+  module.exports.getCalendarEvents = async (event) => {
+    // The values used to instantiate the OAuthClient are at the top of the file
+      const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+      );
+      // Decode authorization code extracted from the URL query
+      const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+      oAuth2Client.setCredentials({ access_token });
+    
+      return new Promise((resolve, reject) => {
+        calendar.events.list(
+          {
+            calendarId: calendar_id,
+            auth: oAuth2Client,
+            timeMin: new Date().toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+          },
+          (error, response) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(response);
+            }
+          }
+        );
+      })
+        .then((token) => {
+          // Respond with OAuth token 
+          return {
+            statusCode: 200,
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({ events: resultingClientExists.data.items }),
+          };
+        })
+        .catch((err) => {
+          // Handle error
+          console.error(err);
+          return {
+            statusCode: 500,
+            body: JSON.stringify(err),
+          };
+        });
+    };
 

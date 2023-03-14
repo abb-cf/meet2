814e379df0fbd7abc8b0a77a2da21310e6adf8e6
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-03-13 23:17:46 -0400

    finishing up testing for unit 4.5, implementing code that accesses full api via axios

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -6,6 +6,7 @@ class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
+        showSuggestions: undefined,
     }
 
     handleInputChanged = (event) => {
@@ -21,8 +22,11 @@ class CitySearch extends Component {
 
     handleItemClicked = (suggestion) => {
         this.setState({
-            query: suggestion
+            query: suggestion,
+            showSuggestions: false,
         });
+
+        this.props.updateEvents(suggestion);
     }
 
   render() {
@@ -33,8 +37,9 @@ class CitySearch extends Component {
                 className="city"
                 value={this.state.query}
                 onChange={this.handleInputChanged}
+                onFocus={() => { this.setState({ showSuggestions: true }) }}
             />
-            <ul className="suggestions">
+            <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
                 {this.state.suggestions.map((suggestion) => (
                     <li 
                         key={suggestion}
@@ -42,7 +47,7 @@ class CitySearch extends Component {
                     >
                     {suggestion}</li>
                 ))}
-                <li>
+                <li onClick={() => this.handleItemClicked("all")}>
                     <b>See all cities</b>
                 </li>
             </ul>

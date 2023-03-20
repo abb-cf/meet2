9b71e9279f8ed61fea08ec12f8824bc8585d731d
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-03-20 10:03:11 -0400

    installed and setup atatus-spa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('ed98f1088d384aa6bad57d889da419b1').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +17,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
+
 

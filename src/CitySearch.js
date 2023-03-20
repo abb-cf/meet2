09433fb177455f6661eaacf80526b5029ef48b3e
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-03-20 16:40:26 -0400

    added div to InfoAlert to keep from showing it on top of the search box

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -45,7 +45,9 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
-        <InfoAlert text={this.state.infoText} />
+        <div>
+            <InfoAlert text={this.state.infoText} />
+        </div>
             <input
                 type="text"
                 className="city"

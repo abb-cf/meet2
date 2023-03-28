ab6a7242d8128d210c120d01fa98be65a8678890
abb-cf <106682621+abb-cf@users.noreply.github.com>
2023-03-28 16:28:14 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -103,22 +103,22 @@ class App extends Component {
         <h4>Events in each city</h4>
         <div className="data-vis-wrapper">
           <EventGenre events={events} />
+        
+          <ResponsiveContainer height={400} >
+            <ScatterChart
+              margin={{
+                top: 20, right: 20, bottom: 20, left: 20,
+              }}
+            >
+              <CartesianGrid />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            
+            </ScatterChart>
+          </ResponsiveContainer>
         </div>
-        <ResponsiveContainer height={400} >
-          <ScatterChart
-            margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
-            }}
-          >
-            <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter data={this.getData()} fill="#8884d8" />
-          
-          </ScatterChart>
-        </ResponsiveContainer>
-
         <EventList events={events} 
         />
         <WelcomeScreen 
